758c14e2a2d616cfd39c7e00a1c1dc12089e2d59
Rafi Rahman <81208207+rfrh@users.noreply.github.com>
2021-08-25 17:07:01 +0700

    Add files via upload

--- latihan-ukl/latihanukl/ukl.java
+++ latihan-ukl/latihanukl/ukl.java
@@ -4,49 +4,45 @@ import java.util.Scanner;
 
 public class ukl {
     public static void main(String[] args) {
+        // Buat copas
+        // System.out.println("");
+        // for(int i=0; i<5; i++){}
+        String[] namapelanggan = { ("Anni"), ("Bima"), ("Dika"), ("Edo"), ("Rina") };
+
         Scanner input = new Scanner(System.in);
-        String[] namapelanggan = { ("Galuh"), ("Indro"), ("Jedi"), ("Kanu") };
-        String[] alamatpelanggan = { ("Sawojajar"), ("Kedung Kandang"), ("Ijen"), ("Dinoyo") };
-        int[] golongan = { 1, 3, 2, 3 };
 
-        int harga1 = 1000;
-        int harga2 = 1300;
-        int harga3 = 1500;
-        int total = 0;
-
-        System.out.println("Masukkad Id Pelanggan : ");
+        System.out.println("Masukkan Nomor Id : ");
         int id = input.nextInt();
 
-        System.out.println("Masukkan Banyak Pemakaian : ");
-        int pemakaian = input.nextInt();
+        System.out.println("Masukkan Penggunaan Air : ");
+        int penggunaan = input.nextInt();
 
-        int penghitungan = golongan[id - 1];
-        switch (penghitungan) {
-            case 1:
-                total += pemakaian * harga1;
-                break;
-            case 2:
-                total += pemakaian * harga2;
-                break;
-            case 3:
-                total += pemakaian * harga3;
-                break;
+        int totalbayar = 6000; // deklarasi sekalian biaya admin
+
+        System.out.println("===============================");
+        System.out.println("Nomor Id = " + id);
+        System.out.println("Nama Pelanggan = " + namapelanggan[id - 1]);
+        System.out.println("Penggunaan Air = " + penggunaan);
+
+        if (penggunaan <= 10) {
+            totalbayar += penggunaan * 3000;
+            System.out.println("Total Tagihan = Rp " + totalbayar);
         }
 
-        if (total < 50000) {
-            total = 50000;
+        else if (penggunaan <= 20) {
+            totalbayar += 10 * 3000 + (penggunaan - 10) * 4000;
+            System.out.println("Total Tagihan = Rp " + totalbayar);
         }
-        
-        total += 13000;
 
-        System.out.println("=====================");
-        System.out.println("Id = " + id);
-        System.out.println("Nama = " + namapelanggan[id - 1]);
-        System.out.println("Golongan = " + golongan[id - 1]);
-        System.out.println("Alamat = " + alamatpelanggan[id - 1]);
-        System.out.println("---------------------");
-        System.out.println("Jumlah Penggunaan = " + pemakaian + " kwh");
-        System.out.println("Total Harus Bayar = Rp " + (total);
+        else if (penggunaan <= 30) {
+            totalbayar += 10 * 3000 + 10 * 4000 + (penggunaan - 20) * 5000;
+            System.out.println("Total Tagihan = Rp " + totalbayar);
+        }
+
+        else if (penggunaan >= 31) {
+            totalbayar += 10 * 3000 + 10 * 4000 + 10 * 5000 + (penggunaan - 30) * 6000;
+            System.out.println("Total Tagihan = Rp " + totalbayar);
+        }
 
     }
 }
